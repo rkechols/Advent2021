e79d1634095dfb11a449d9e1c41b5730f306e022
Ryan Echols <rkechols21@gmail.com>
2021-12-17 02:14:52 -0700

    attempt at part 2; right answer for 1, wrong answer for 2

--- day05_hydrothermal.py
+++ day05_hydrothermal.py
@@ -1,10 +1,12 @@
 import os
 import re
+from itertools import repeat
 from typing import Dict, Tuple
 
 from constants import INPUTS_DIR, UTF_8
 
 
+USE_DIAGONALS = True  # False for part 1, True for part 2
 INPUT_FILE = os.path.join(INPUTS_DIR, "input05.txt")
 
 LINE_RE = re.compile(r"(\d+),(\d+) -> (\d+),(\d+)")
@@ -22,27 +24,35 @@ if __name__ == "__main__":
     # count spots for each tuple
     counter: Dict[Tuple[int, int], int] = dict()
     for (x1, y1), (x2, y2) in lines:
-        if x1 == x2:
-            if y1 < y2:
-                y_range = range(y1, y2 + 1)
-            else:
-                y_range = range(y2, y1 + 1)
-            for y in y_range:
-                try:
-                    counter[(x1, y)] += 1
-                except KeyError:
-                    counter[(x1, y)] = 1
-        elif y1 == y2:
-            if x1 < x2:
-                x_range = range(x1, x2 + 1)
-            else:
-                x_range = range(x2, x1 + 1)
-            for x in x_range:
-                try:
-                    counter[(x, y1)] += 1
-                except KeyError:
-                    counter[(x, y1)] = 1
-        # TODO: else
+        if x1 == x2 and y1 == y2:
+            # special case of a single point so we don't get in an infinite loop
+            try:
+                counter[(x1, y1)] += 1
+            except KeyError:
+                counter[(x1, y1)] = 1
+        # skip a diagonal if we need to
+        if not USE_DIAGONALS and x1 != x2 and y1 != y2:
+            continue
+        # create an x iterable
+        if x1 < x2:
+            x_range = range(x1, x2 + 1)
+        elif x2 < x1:
+            x_range = range(x2, x1 + 1)
+        else:
+            x_range = repeat(x1)
+        # create a y iterable
+        if y1 < y2:
+            y_range = range(y1, y2 + 1)
+        elif y2 < y1:
+            y_range = range(y2, y1 + 1)
+        else:
+            y_range = repeat(y1)
+        # count the points
+        for x, y in zip(x_range, y_range):
+            try:
+                counter[(x, y)] += 1
+            except KeyError:
+                counter[(x, y)] = 1
     overlap_count = 0
     for count in counter.values():
         if count >= 2:
