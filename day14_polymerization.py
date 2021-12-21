add9d99f1c2bcf40b5b18acffa66b0af5193d758
Ryan Echols <rkechols21@gmail.com>
2021-12-20 17:12:08 -0700

    better way to do day 14; solved both parts

--- day14_polymerization.py
+++ day14_polymerization.py
@@ -1,9 +1,7 @@
 import os
 import re
-from collections import Counter
 
 from constants import INPUTS_DIR, UTF_8
-from tqdm import tqdm
 
 
 INPUT_FILE = os.path.join(INPUTS_DIR, "input14.txt")
@@ -15,7 +13,7 @@ N_STEPS = 40  # 10 for part 1; 40 for part 2
 if __name__ == "__main__":
     rules = dict()
     with open(INPUT_FILE, "r", encoding=UTF_8) as f:
-        polymer = list(f.readline().strip())
+        polymer = f.readline().strip()
         f.readline()  # skip the blank
         while (line_ := f.readline()) != "":
             line = line_.strip()
@@ -24,21 +22,52 @@ if __name__ == "__main__":
                 raise ValueError(f"Could not match line:\n{line}")
             pair, result = match.groups()
             rules[pair] = result
-    # synthesize
-    for _ in tqdm(range(N_STEPS)):
-        edited = list()
-        for i in range(len(polymer) - 1):
-            edited.append(polymer[i])
-            pair = polymer[i] + polymer[i + 1]
+    # initialize
+    pair_counts = dict()
+    for i in range(len(polymer) - 1):
+        pair = polymer[i:i+2]
+        try:
+            pair_counts[pair] += 1
+        except KeyError:
+            pair_counts[pair] = 1
+    # synthesize (keeping counts of each char-pair type)
+    for _ in range(N_STEPS):
+        pair_counts_new = dict()
+        for pair, count in pair_counts.items():
             try:
                 to_insert = rules[pair]
-                edited.append(to_insert)
             except KeyError:
-                pass  # no matching rule; no insertion
-        edited.append(polymer[-1])
-        polymer = edited
-    # analyze
-    char_counts = Counter(polymer)
+                # no replacement; all instances stay unchanged
+                pair_counts_new[pair] = count
+            else:
+                # left char with inserted
+                new_pair1 = pair[0] + to_insert
+                try:
+                    pair_counts_new[new_pair1] += count
+                except KeyError:
+                    pair_counts_new[new_pair1] = count
+                # inserted with right char
+                new_pair2 = to_insert + pair[1]
+                try:
+                    pair_counts_new[new_pair2] += count
+                except KeyError:
+                    pair_counts_new[new_pair2] = count
+        # update 'current'
+        pair_counts = pair_counts_new
+    # count how many of each individual char
+    char_counts = dict()
+    for pair, count in pair_counts.items():
+        first_char = pair[0]  # only the first is needed since including the second would double-count it
+        try:
+            char_counts[first_char] += count
+        except KeyError:
+            char_counts[first_char] = count
+    # the last character gets missed if you just go by the pairs
+    try:
+        char_counts[polymer[-1]] += 1
+    except KeyError:
+        char_counts[polymer[-1]] = 1
+    # what are the most and least frequent?
     freq_biggest = None
     freq_smallest = None
     for count in char_counts.values():
