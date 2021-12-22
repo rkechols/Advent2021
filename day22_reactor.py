d21f37db9f84f98e0a06ba8e6db7dff4c8dcfedf
Ryan Echols <rkechols21@gmail.com>
2021-12-22 03:47:19 -0700

    solved day 22 part 1

---
+++ day22_reactor.py
@@ -0,0 +1,42 @@
+import os
+import numpy as np
+import re
+
+from constants import INPUTS_DIR, UTF_8
+
+
+INPUT_FILE = os.path.join(INPUTS_DIR, "input22.txt")
+
+RANGE_RE = re.compile(r"(-?\d+)\.\.(-?\d+)")
+ON = "on"
+
+SIZE = 101
+SHIFT = 50
+
+
+if __name__ == "__main__":
+    grid = np.zeros((SIZE, SIZE, SIZE), dtype=bool)
+    with open(INPUT_FILE, "r", encoding=UTF_8) as f:
+        for line_ in f:
+            on_off, loc = line_.strip().split()
+            power = (on_off == ON)
+            x_loc_str, y_loc_str, z_loc_str = loc.split(",")
+            x_low, x_high = map(int, RANGE_RE.search(x_loc_str).groups())
+            x_low += SHIFT
+            x_high += SHIFT + 1
+            if x_low < 0 or x_high >= SIZE:
+                continue  # skip
+            y_low, y_high = map(int, RANGE_RE.search(y_loc_str).groups())
+            y_low += SHIFT
+            y_high += SHIFT + 1
+            if y_low < 0 or y_high >= SIZE:
+                continue  # skip
+            z_low, z_high = map(int, RANGE_RE.search(z_loc_str).groups())
+            z_low += SHIFT
+            z_high += SHIFT + 1
+            if z_low < 0 or z_high >= SIZE:
+                continue  # skip
+            # perform the switch
+            grid[x_low:x_high, y_low:y_high, z_low:z_high] = power
+    n_spots_on = grid.sum()
+    print(f"NUMBER cubes ON: {n_spots_on}")
