a2a8e6a4379c30918df47c0a3bf4f3d726ee6154
Ryan Echols <rkechols21@gmail.com>
2021-12-27 20:43:36 -0600

    more efficient; still bad

--- day24_alu.py
+++ day24_alu.py
@@ -1,6 +1,7 @@
 import os
 import re
-from typing import Dict, List
+from typing import Dict, List, Optional
+from copy import copy, deepcopy
 
 from constants import INPUTS_DIR, UTF_8
 
@@ -24,62 +25,60 @@ class Variable:
     def __init__(self, value: int):
         self.value = value
 
+    def __repr__(self) -> str:
+        return f"Variable({self.value})"
+
 
 def get_variables() -> Dict[str, Variable]:
     return {letter: Variable(0) for letter in "wxyz"}
 
 
-def evaluate(instruction: str, variables: Dict[str, Variable], next_digit_index: int, digits: List[int]) -> bool:
-    if (inp_match := INP_RE.fullmatch(instruction)) is not None:
-        receiver_var = inp_match.group(1)
-        receiver_var = variables[receiver_var]
-        receiver_var.value = digits[next_digit_index]
-        return True
+def evaluate(instruction: str, variables: Dict[str, Variable]):
+    match = BINARY_OP_RE.fullmatch(instruction)
+    command, receiver_var, val = match.groups()
+    receiver_var = variables[receiver_var]
+    try:
+        val = int(val)
+    except ValueError:
+        val = variables[val].value
+    if command == ADD:
+        receiver_var.value += val
+    elif command == MUL:
+        receiver_var.value *= val
+    elif command == DIV:
+        receiver_var.value = int(receiver_var.value / val)
+    elif command == MOD:
+        receiver_var.value = int(receiver_var.value % val)
+    elif command == EQL:
+        receiver_var.value = int(receiver_var.value == val)
     else:
-        match = BINARY_OP_RE.fullmatch(instruction)
-        command, receiver_var, val = match.groups()
-        receiver_var = variables[receiver_var]
-        try:
-            val = int(val)
-        except ValueError:
-            val = variables[val].value
-        if command == ADD:
-            receiver_var.value += val
-        elif command == MUL:
-            receiver_var.value *= val
-        elif command == DIV:
-            receiver_var.value = int(receiver_var.value / val)
-        elif command == MOD:
-            receiver_var.value = int(receiver_var.value % val)
-        elif command == EQL:
-            receiver_var.value = int(receiver_var.value == val)
+        raise ValueError(f"invalid command: \"{instruction}\"")
+
+
+def validate_dfs(variables: Dict[str, Variable], instructions: List[str], instruction_index: int = 0, digits: List[int] = None) -> Optional[List[int]]:
+    if digits is None:
+        digits = list()
+    n = len(instructions)
+    while instruction_index < n:
+        instruction = instructions[instruction_index]
+        instruction_index += 1
+        if (inp_match := INP_RE.fullmatch(instruction)) is not None:
+            receiver_var_name = inp_match.group(1)
+            for digit in range(9, 0, -1):
+                new_vars = deepcopy(variables)
+                receiver_var = new_vars[receiver_var_name]
+                receiver_var.value = digit
+                new_digits = copy(digits)
+                new_digits.append(digit)
+                answer = validate_dfs(new_vars, instructions, instruction_index, new_digits)
+                if answer is not None:
+                    return answer
         else:
-            raise ValueError(f"invalid command: \"{instruction}\"")
-        return False
-
-
-def validate(instructions: List[str], digits: List[int]) -> bool:
-    variables = get_variables()
-    next_digit_index = 0
-    for instruction in instructions:
-        if evaluate(instruction, variables, next_digit_index, digits):
-            next_digit_index += 1
-    valid = (variables["z"] == 0)
-    return valid
-
-
-def generate_options():
-    digits = [9 for _ in range(N_DIGITS)]
-    while True:
-        for i in range(N_DIGITS - 1, -1, -1):
-            if digits[i] != 1:
-                digits[i] -= 1
-                break  # yield then restart the while-loop
-            else:  # rollover and move to the next digit
-                digits[i] = 9
-        else:  # all the digits were 1; end of generation
-            break  # exit the while-loop
-        yield digits
+            evaluate(instruction, variables)
+    if variables["z"] == 0:
+        return digits
+    else:
+        return None
 
 
 if __name__ == "__main__":
@@ -88,12 +87,7 @@ if __name__ == "__main__":
         for line_ in f:
             instructions_.append(line_.strip())
     # part 1
-    number = None
-    for digits_ in generate_options():
-        valid_ = validate(instructions_, digits_)
-        if valid_:
-            number = "".join(str(d) for d in digits_)
-            break
-    else:
-        raise ValueError("No valid number?")
+    variables_ = get_variables()
+    answer_ = validate_dfs(variables_, instructions_)
+    number = "".join(str(d) for d in answer_)
     print(f"HIGHEST valid number: {number}")
