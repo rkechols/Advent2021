5ead3fc51d3b96bfb2f98feeb7053992a298f2eb
Ryan Echols <rkechols21@gmail.com>
2021-12-27 23:06:20 -0600

    another attempt at day 24; doesn't work

--- day24_alu.py
+++ day24_alu.py
@@ -1,7 +1,6 @@
 import os
 import re
-from typing import Dict, List, Optional
-from copy import copy, deepcopy
+from typing import Dict, List, Tuple
 
 from constants import INPUTS_DIR, UTF_8
 
@@ -11,74 +10,68 @@ INPUT_FILE = os.path.join(INPUTS_DIR, "input24.txt")
 INP_RE = re.compile(r"inp ([wxyz])")
 BINARY_OP_RE = re.compile(r"(add|mul|div|mod|eql) ([wxyz]) ([wxyz]|-?\d+)")
 
-INPUT = "inp"
-ADD = "add"
-MUL = "mul"
-DIV = "div"
-MOD = "mod"
-EQL = "eql"
-
 N_DIGITS = 14
 
-
-class Variable:
-    def __init__(self, value: int):
-        self.value = value
-
-    def __repr__(self) -> str:
-        return f"Variable({self.value})"
-
-
-def get_variables() -> Dict[str, Variable]:
-    return {letter: Variable(0) for letter in "wxyz"}
+VAR_TO_INDEX = {char: i_ for i_, char in enumerate("wxyz")}
+OP_TO_FUNCTION = {
+    "add": lambda x, y: x + y,
+    "mul": lambda x, y: x * y,
+    "div": lambda x, y: int(x / y),
+    "mod": lambda x, y: int(x % y),
+    "eql": lambda x, y: int(x == y)
+}
 
 
-def evaluate(instruction: str, variables: Dict[str, Variable]):
+def evaluate(instruction: str, variables: Dict[Tuple[int, ...], str]) -> Dict[Tuple[int, ...], str]:
     match = BINARY_OP_RE.fullmatch(instruction)
     command, receiver_var, val = match.groups()
-    receiver_var = variables[receiver_var]
-    try:
-        val = int(val)
-    except ValueError:
-        val = variables[val].value
-    if command == ADD:
-        receiver_var.value += val
-    elif command == MUL:
-        receiver_var.value *= val
-    elif command == DIV:
-        receiver_var.value = int(receiver_var.value / val)
-    elif command == MOD:
-        receiver_var.value = int(receiver_var.value % val)
-    elif command == EQL:
-        receiver_var.value = int(receiver_var.value == val)
-    else:
-        raise ValueError(f"invalid command: \"{instruction}\"")
+    func = OP_TO_FUNCTION[command]
+    receiver_var_index = VAR_TO_INDEX[receiver_var]
+    new_variables = dict()
+    for var_tup, value_s in variables.items():
+        try:
+            val = int(val)
+        except ValueError:
+            val = var_tup[VAR_TO_INDEX[val]]
+        var_list = list(var_tup)
+        var_list[receiver_var_index] = func(var_tup[receiver_var_index], val)
+        new_variables[tuple(var_list)] = value_s
+    return new_variables
 
 
-def validate_dfs(variables: Dict[str, Variable], instructions: List[str], instruction_index: int = 0, digits: List[int] = None) -> Optional[List[int]]:
-    if digits is None:
-        digits = list()
-    n = len(instructions)
-    while instruction_index < n:
-        instruction = instructions[instruction_index]
-        instruction_index += 1
-        if (inp_match := INP_RE.fullmatch(instruction)) is not None:
-            receiver_var_name = inp_match.group(1)
-            for digit in range(9, 0, -1):
-                new_vars = deepcopy(variables)
-                receiver_var = new_vars[receiver_var_name]
-                receiver_var.value = digit
-                new_digits = copy(digits)
-                new_digits.append(digit)
-                answer = validate_dfs(new_vars, instructions, instruction_index, new_digits)
-                if answer is not None:
-                    return answer
-        else:
-            evaluate(instruction, variables)
-    if variables["z"] == 0:
-        return digits
-    else:
-        return None
+def solve_greatest(instructions: List[str]) -> int:
+    variables = {(0, 0, 0, 0): ""}
+    for instruction in instructions:
+        if (match := INP_RE.fullmatch(instruction)) is not None:
+            # input command gets special treatment
+            receiver_var = match.group(1)
+            receiver_var_index = VAR_TO_INDEX[receiver_var]
+            new_variables = dict()
+            for var_tup, value_s in variables.items():
+                for i in range(1, 10):  # branch out
+                    var_list = list(var_tup)
+                    var_list[receiver_var_index] = i
+                    new_tup = tuple(var_list)
+                    new_value_s = f"{value_s}{i}"
+                    if new_tup in new_variables:
+                        # which is bigger?
+                        new_value = int(new_value_s)
+                        if new_value > int(new_variables[new_tup]):
+                            new_variables[new_tup] = new_value_s
+                    else:
+                        new_variables[new_tup] = new_value_s
+            variables = new_variables
+        else:  # just execute the command
+            variables = evaluate(instruction, variables)
+    # find the biggest with a 0 for z
+    z_index = VAR_TO_INDEX["z"]
+    biggest = None
+    for var_tup, value_s in variables.items():
+        if var_tup[z_index] == 0:
+            value = int(value_s)
+            if biggest is None or value > biggest:
+                biggest = value
+    return biggest
 
 
 if __name__ == "__main__":
@@ -87,7 +80,5 @@ if __name__ == "__main__":
         for line_ in f:
             instructions_.append(line_.strip())
     # part 1
-    variables_ = get_variables()
-    answer_ = validate_dfs(variables_, instructions_)
-    number = "".join(str(d) for d in answer_)
+    number = solve_greatest(instructions_)
     print(f"HIGHEST valid number: {number}")
